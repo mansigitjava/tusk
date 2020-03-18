4276538fac570328c7990dbd763decd4a8f92383
mansigitjava <60467120+mansigitjava@users.noreply.github.com>
2020-03-18 14:35:22 +0530

    Create NewJavaFile
    I commit it to do practice in java

---
+++ NewJavaFile.java
@@ -0,0 +1,11 @@
+Class Practice
+{
+ String name= "manbsi";
+ int ag e= 23;
+ }
+ Class Main
+ {
+   public static void main(string args[])
+   { 
+       System.out.println("name"+name, "age" +age);
+       }
